dd6651f606b7678b9be53f6630b0263ee81c6b53
Replit user <>
2023-03-31 18:14:13 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -13,24 +13,39 @@ typedef struct ArrayList {
 //1
 ArrayList *createList(void) {
   ArrayList * list = (ArrayList *)malloc(sizeof(ArrayList));
-  list->data = (void **)malloc(2*sizeof(void**));
+  list->data = (void **) malloc(2*sizeof(void**));
   list->size = 0;
   list->capacity = 2;
   return list;
 }
 
+//2
 void append(ArrayList * l, void * data){
   if(l->size == l->capacity){
     l->capacity *= 2;
-    l->data = realloc(l->data,l->capacity*sizeof(void**));
+    l->data = realloc(l->data, l->capacity*sizeof(void**));
   }
   
   l->data[l->size] = data;
   l->size++;
 }
 
+//3
 void push(ArrayList * l, void * data, int i){
+  if(i > l->size) return;
+  
+  if(l->size == l->capacity){
+    l->capacity *= 2;
+    l->data = realloc(l->data, l->capacity*sizeof(void**));
+  }
 
+  for(int j = l->size ; j > i ; j--)
+  {
+    l->data[j] = l->data[j-1];
+  }
+
+  l->data[i] = data;
+  l->size++;
 }
 
 void* pop(ArrayList * l, int i){
